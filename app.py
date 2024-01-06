af4be831ef1251ce1e4c846c36ee2707e63d3252
Anand <Bavk263@gmail.com>
2024-01-06 09:35:05 +0530

    Adim init for car and bike data input

---
+++ app.py
@@ -0,0 +1,57 @@
+from pyrebase import * 
+from flask import * 
+
+config = {
+
+  "apiKey": "AIzaSyDZmiSBPCUpRfCkegjATrEe1057S4j_r1Q",
+  "authDomain": "chargeease-21f68.firebaseapp.com",
+  "databaseURL": "https://chargeease-21f68-default-rtdb.firebaseio.com",
+  "projectId": "chargeease-21f68",
+  "storageBucket": "chargeease-21f68.appspot.com",
+  "messagingSenderId": "255861292061",
+  "appId": "1:255861292061:web:2ae5ca02d54a45419a5af4",
+  "measurementId": "G-R1Y1JTCJ42"
+
+}
+
+firebase = initialize_app(config)
+
+db = firebase.database()
+
+app = Flask(__name__) 
+#name = "akhil"
+#print(name)
+#db.child("ChargeEase").child("EVUser").push({"id":"EV01","name":name,"age":21,"email":"bandianand900@gmail.com","password":"Bavk@gmail.com","Vechile":"C-name-type","Battery":0,"Range":0,"mobile":"6302174710"})
+
+@app.route('/',methods=['GET'])
+def choose():
+  #db.child("ChargeEase").child("Bike").push({"bikeModel":"","bikeType":"","power":"","range":"","modes":""})
+  return render_template('index.htm')
+@app.route('/cars',methods=['GET','POST'])
+def cars():
+  if request.method == 'POST':
+    carModel = request.form['carModel']
+    carType = request.form['carType']
+    power = request.form['power']
+    range1 = request.form['range']
+    modes = request.form['modes']
+    db.child("ChargeEase").child("Cars").push({"carModel":carModel,"carType":carType,"power":power,"range":range1,"modes":modes})
+  data = db.child("ChargeEase").child("Cars").get()
+  l = data.val()
+  return render_template('CarsForm.html',data=l)
+
+@app.route('/bike',methods=['GET','POST'])
+def bike():
+  if request.method == 'POST':
+    carModel = request.form['bikeModel']
+    carType = request.form['bikeType']
+    power = request.form['power']
+    range1 = request.form['range']
+    modes = request.form['modes']
+    db.child("ChargeEase").child("Bike").push({"bikeModel":carModel,"bikeType":carType,"power":power,"range":range1,"modes":modes})
+  data = db.child("ChargeEase").child("Bike").get()
+  l = data.val()
+  return render_template('BikeForm.html',data=l)
+
+if __name__ == '__main__':
+  app.run(debug=True)
