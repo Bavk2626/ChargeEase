6ef14f82c482da0cecf081b59226908060ef31f7
Anand <Bavk263@gmail.com>
2024-03-05 11:36:46 +0530

    styling and things are done

--- app.py
+++ app.py
@@ -17,17 +17,51 @@ config = {
 firebase = initialize_app(config)
 
 db = firebase.database()
+auth = firebase.auth()
+person = {"present": False}
 
 app = Flask(__name__) 
+app.secret_key = '_5#y2L"F4Q8z\n\xec]/'
 #name = "akhil"
 #print(name)
 #db.child("ChargeEase").child("EVUser").push({"name":name,"age":21,"email":"bandianand900@gmail.com","status":0,"Vechile":"C-name-type","mobile":"6302174710","address":"plot 126"})
 
 @app.route('/',methods=['GET'])
-def choose():
+def home():
   #db.child("ChargeEase").child("Bike").push({"bikeModel":"","bikeType":"","power":"","range":"","batteryCap":"","chargingTime":""})
   #db.child("ChargeEase").child("Car").push({"carModel":"","carType":"","power":"","range":"","batteryCap":"","chargingTime":""})
-  return render_template('index.htm')
+  return render_template('home.html',person=person)
+@app.route('/login',methods=['POST','GET'])
+def login():
+  return render_template("Login.html", person=person)
+
+@app.route("/result",methods=["POST","GET"])
+def result():
+  if request.method ==  'POST':
+    #print(request.form)
+    email = request.form['email']
+    password = request.form['password']
+    try :
+      user = auth.sign_in_with_email_and_password(email,password)
+      #verify = auth.get_account_info(user['idToken'])['users'][0]['emailVerified']
+      
+      f = 0
+      if not ("xPK2YsHozMN9vkBp4lcax83jlAr2" == user['localId']):
+        f = 1
+        raise Exception("Only admin can login")
+      person["id"] = user['localId']
+      person['present'] = True
+      #print(person)
+    except Exception as e:
+      #s = e.args[1]
+      if f == 1 :
+        flash("Only admin can login",'danger')
+      else :  
+        flash("Invalid Email or Password",'danger')
+      return redirect(url_for('login'))
+
+    return  redirect(url_for('Evdata'))
+
 @app.route('/edit',methods=['GET','POST'])
 def edit():
    if request.method == 'POST':
@@ -36,6 +70,7 @@ def edit():
     Latitude = request.form['lat']
     Longitude = request.form['lng']
     idd = request.form['id']
+    avl = request.form['avl']
     print(request.form)
     if 'rapid' in request.form:
       Rapid = 1
@@ -57,18 +92,26 @@ def edit():
     station['address'] = Address
     station['lat'] = Latitude
     station['lng'] = Longitude
+    station['availability'] = avl
     station['rapid'] = Rapid
     station['batteryexchange'] = BatteryExchange
     station['solar'] = Solar
     ref = db.child("ChargeEase").child("EVStation").child(idd)
     ref.update(station)
     return redirect(url_for('Evdata'))
-@app.route('/delete//<string:user_id>',methods=['GET','POST'])
-def delete():
 
-  return "mama"
+@app.route('/delete//<string:user_id>//<string:name>',methods=['GET','POST'])
+def delete(user_id,name):
+  ref = db.child("ChargeEase").child("EVStation").child(user_id)
+  ref.remove()
+  return name + " Station is Deleted"
+
+
 @app.route('/cars',methods=['GET','POST'])
 def cars():
+  if not person['present']:
+    return redirect(url_for('login'))
+
   if request.method == 'POST':
     carModel = request.form['carModel']
     carType = request.form['carType']
@@ -79,28 +122,38 @@ def cars():
     db.child("ChargeEase").child("Car").push({"carModel":carModel,"carType":carType,"power":power,"range":range1,"batteryCap":batteryCap,"chargingTime":chargingTime})
   data = db.child("ChargeEase").child("Car").get()
   l = data.val()
-  return render_template('CarsForm.html',data=l)
+  return render_template('CarsForm.html',data=l,person=person)
 
 @app.route('/bike',methods=['GET','POST'])
 def bike():
-  if request.method == 'POST':
-    carModel = request.form['bikeModel']
-    carType = request.form['bikeType']
-    power = request.form['power']
-    range1 = request.form['range']
-    batteryCap = request.form['batteryCap']
-    chargingTime = request.form['chargingTime']
-    db.child("ChargeEase").child("Bike").push({"bikeModel":carModel,"bikeType":carType,"power":power,"range":range1,"batteryCap":batteryCap,"chargingTime":chargingTime})
-  data = db.child("ChargeEase").child("Bike").get()
-  l = data.val()
-  return render_template('BikeForm.html',data=l)
+  if not person['present']:
+    return redirect(url_for('login'))
+  try :
+    if request.method == 'POST':
+      carModel = request.form['bikeModel']
+      carType = request.form['bikeType']
+      power = request.form['power']
+      range1 = request.form['range']
+      batteryCap = request.form['batteryCap']
+      chargingTime = request.form['chargingTime']
+      db.child("ChargeEase").child("Bike").push({"bikeModel":carModel,"bikeType":carType,"power":power,"range":range1,"batteryCap":batteryCap,"chargingTime":chargingTime})
+    data = db.child("ChargeEase").child("Bike").get()
+    l = data.val()
+    return render_template('BikeForm.html',data=l,person=person)
+  except Exception as e :
+    return "Connection error"
 
 @app.route('/EVdata',methods=['GET','POST'])
 def Evdata():
-  #db.child("ChargeEase").child("Bik").push({"bikeModel":carModel,"bikeType":carType,"power":power,"range":range1,"batteryCap":batteryCap,"chargingTime":chargingTime})
-  data = db.child("ChargeEase").child("EVStation").get()
-  l = data.val()
-  return render_template('EVdata.html',data=l)
+  if not person['present']:
+    return redirect(url_for('login'))
+  try :
+    #db.child("ChargeEase").child("Bik").push({"bikeModel":carModel,"bikeType":carType,"power":power,"range":range1,"batteryCap":batteryCap,"chargingTime":chargingTime})
+    data = db.child("ChargeEase").child("EVStation").get()
+    l = data.val()
+    return render_template('EVdata.html',data=l,person=person)
+  except Exception as e :
+    return "Connection Error"
 
 
 @app.route("/update//<string:user_id>")
@@ -122,6 +175,17 @@ def approval(user_id):
   except Exception as e:
         print(f"Error: {e}") 
   return "okay" 
+
+
+
+
+@app.route("/logout")
+def logout():
+  global person 
+  #print("logout :", person)
+  person = {"present": False}
+  return redirect(url_for("home"))
+
 
 
 if __name__ == '__main__':
